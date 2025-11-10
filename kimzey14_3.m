1ac279a6c56d6884b7e7b14478ad5a19406a322a
Alexander <0869801@my.scccd.edu>
2025-11-10 22:06:33 +0000

    last practice

--- kimzey14_3.m
+++ kimzey14_3.m
@@ -1,1 +1,9 @@
 
+%Integral and derivative of a polynomial
+p=input("Enter the coefficients of a cubic polynomial, as a matrix[..]\n");
+y0=input("Enter the integration constant for the function.\n");
+pDer=polyder(p);
+pint=polyint(p,y0);
+%Report the output
+fprintf("The derivative of the function is %gx^2+%gx+%g.\n",pDer)
+fprintf("The intergal of the function is %gx^4+%gx^3+%g^2+%gx+%g.\n",pint)
